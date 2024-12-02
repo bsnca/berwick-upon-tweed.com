9c40aff6505cc8d659c721576dcba413de3832c0
go5media <githubaccount@go5media.com>
2024-12-02 01:52:40 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-// Function to change the background color when the button is clicked
-document.getElementById('changeColorButton').addEventListener('click', function() {
-    // Random color generator
-    const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
-    document.body.style.backgroundColor = randomColor;
+// Select all images in the gallery
+const galleryImages = document.querySelectorAll('.gallery img');
+
+// Add click event to each image
+galleryImages.forEach(image => {
+    image.addEventListener('click', () => {
+        // Toggle the enlarged class
+        if (image.classList.contains('enlarged')) {
+            image.classList.remove('enlarged'); // Remove enlarged view
+        } else {
+            // Remove enlarged class from other images
+            galleryImages.forEach(img => img.classList.remove('enlarged'));
+            image.classList.add('enlarged'); // Add enlarged view
+        }
+    });
 });
